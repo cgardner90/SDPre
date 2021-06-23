45caba77e7686340cc53a151ca2b09ed9b40e712
Caleb <calebgardner2015@gmail.com>
2021-06-22 18:39:43 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name= "Caleb Gardner";
+    int age = 31;
+    String state = "South Carolina";
+    System.out.println("Name: " + name + " Age: " + age+ " State: " + state);
+
+  }
+
+
+}
