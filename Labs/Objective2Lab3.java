01bb2bc034c186b879953f993b7f53259420895a
Caleb <calebgardner2015@gmail.com>
2021-06-23 08:58:10 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String [] args) {
+    double num1, num2, num3, average;
+    num1 = 15.5;
+    num2 = 6.7;
+    num3 = 12.0;
+
+    average = (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
