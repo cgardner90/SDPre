f9d5c4fe1609ca4a8856d0a3592b09becba949da
Caleb <calebgardner2015@gmail.com>
2021-06-27 10:34:27 -0600

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1{
+  public static void main(String[] args){
+    int i;
+
+    for(i=0; i<=10; i++)
+      System.out.println(i);
+    for(i=10; i>=0; i--)
+      System.out.println(i);
+}
+}
