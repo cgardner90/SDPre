952cb814b033698bd455343446e5a4298e19ec0e
Caleb <calebgardner2015@gmail.com>
2021-06-27 09:56:47 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int currentSum = 0, counter = 0;
+
+    while(counter <= 20){
+      
+      currentSum = currentSum + counter;
+      counter += 1;
+
+    }
+    System.out.println(currentSum);
+  }
+}
