c0e7255094d13461299d4018a66b1690bf7d3d28
Caleb <calebgardner2015@gmail.com>
2021-07-05 11:39:17 -0600

    complete Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3{
+  public static void main(String[] args){
+    int counter=0;
+    for(int i=1; i<21; i++){
+      System.out.print(i);
+      counter++;
+      if(counter%2!=0)
+      System.out.println(" is odd");
+      else
+      System.out.println(" is even");
+
+
+    }
+  }
+}
